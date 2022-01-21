4349c4a8100441be32c97a29f70dcd7681308c4f
tanishab557 <98122051+tanishab557@users.noreply.github.com>
2022-01-21 12:48:01 +0530

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -9,7 +9,7 @@ import logging
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
-
+redis_password = os.getenv('REDIS_PASSWORD',"Dogs")
 app = Flask(__name__)
 
 gunicorn_error_logger = logging.getLogger('gunicorn.error')
@@ -18,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host="redis", db=0,password=redis_password ,socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
